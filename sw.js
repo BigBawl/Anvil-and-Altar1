896cd16d4e38e0c6a423c385b0763e8cc0e32786
BigBawl <taylor.mitchell.green@gmail.com>
2026-09-06 22:19:32 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,57 @@
+var VERSION = 'aa-v1';
+var SHELL = VERSION + '-shell';
+var PRECACHE = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(SHELL)
+      .then(function (c) { return c.addAll(PRECACHE); })
+      .catch(function () {})
+      .then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        if (k.indexOf(VERSION) !== 0) return caches.delete(k);
+      }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  var req = e.request;
+  if (req.method !== 'GET') return;
+  var isHTML = req.mode === 'navigate' ||
+    (req.headers.get('accept') || '').indexOf('text/html') !== -1;
+
+  if (isHTML) {
+    e.respondWith(
+      fetch(req).then(function (res) {
+        var copy = res.clone();
+        caches.open(SHELL).then(function (c) { c.put('./index.html', copy); });
+        return res;
+      }).catch(function () {
+        return caches.match('./index.html').then(function (hit) {
+          return hit || caches.match('./');
+        });
+      })
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(req).then(function (hit) {
+      if (hit) return hit;
+      return fetch(req).then(function (res) {
+        if (res && (res.status === 200 || res.type === 'opaque')) {
+          var copy = res.clone();
+          caches.open(SHELL).then(function (c) { c.put(req, copy); });
+        }
+        return res;
+      }).catch(function () { return hit; });
+    })
+  );
+});
